274a588544b89a3eb07e7505a5adc6419a125450
ashifanassar <ashifanassar@gmail.com>
2024-07-11 15:27:10 +0530

    the shell scripting

---
+++ 01.shebang.sh
@@ -0,0 +1,5 @@
+#The shebang notion is where the execution of the script starts it tells the kernel what language the script is and how it has to be worked upon
+
+echo Welcome to shell scripting
+
+echo "Welcome to the Hello world"
